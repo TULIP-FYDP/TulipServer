6ff7c03e21355aa5748bb2f2a70e3df456a74ab5
Nick Manna <manna422@gmail.com>
2018-03-11 01:13:39 -0500

    Added express to broadcast sample

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+// Library Imports
+const express = require('express');
+const http = require('http');
+const url = require('url');
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8080 });
+const app = express();
+
+// app.use(function (req, res) {
+//     res.send({ msg: "hello" });
+// });
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
 
 // Broadcast to all.
 wss.broadcast = function broadcast(data) {
@@ -20,4 +34,8 @@ wss.on('connection', function connection(ws) {
             }
         });
     });
+});
+
+server.listen(8080, function listening() {
+    console.log('Listening on %d', server.address().port);
 });
