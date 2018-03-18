adf186f3fa8a2a043eb4aa9bf71889341dfe5aca
Nick Manna <manna422@gmail.com>
2018-03-18 17:04:39 -0400

    removed stubbed broadcast

--- index.js
+++ index.js
@@ -11,16 +11,6 @@ app.get('/', (req, res) => res.send('Relay Server Running'))
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-
-// Broadcast to all.
-wss.broadcast = function broadcast(data) {
-    wss.clients.forEach(function each(client) {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(data);
-        }
-    });
-};
-
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(data) {
         // Broadcast to everyone else.
